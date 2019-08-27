63751f3ce4ca26871cad7aee6b81dc5d4e58a86c
ke_li <stokid@126.com>
2019-08-27 10:56:38 +0800

    修改引入

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,14 +2,14 @@
 // Project: [LIBRARY_URL_HERE] 
 // Definitions by: [YOUR_NAME_HERE] <[YOUR_URL_HERE]> 
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
-import bodyanalysis from './AipBodyAnalysis';
-import imageSearch from './AipImageSearch';
-import imageClassify from './AipImageClassify';
-import contentCensor from './AipContentCensor';
-import face from './AipFace';
-import orc from './AipOcr';
-import nlp from './AipNlp';
-import kg from './AipKg';
-import speech from './AipSpeech';
-import easydl from './AipEasyDL';
-import httpclient  from './http/httpClient';
+import * as bodyanalysis from './AipBodyAnalysis';
+import * as imageSearch from './AipImageSearch';
+import * as imageClassify from './AipImageClassify';
+import * as contentCensor from './AipContentCensor';
+import * as face from './AipFace';
+import * as orc from './AipOcr';
+import * as nlp from './AipNlp';
+import * as kg from './AipKg';
+import * as speech from './AipSpeech';
+import * as easydl from './AipEasyDL';
+import * as httpclient from './http/httpClient';
